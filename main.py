9e2f2b511230dd70b45cd81fa57be97404decbe2
KismatovKonstantin <53231211+KismatovKonstantin@users.noreply.github.com>
2022-01-21 12:02:10 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,65 @@
-from random import shuffle
 from tkinter import *
-import time
-import string
-
-BOARD_SIZE = 4
-SQUARE_SIZE = 140
-EMPTY_SQUARE = BOARD_SIZE ** 2
+import sys
 
 problems = [
-    "9*9",
-    "9*8",
-    "9*7",
-    "8*8",
-    "8*7",
-    "8*6",
-    "7*7",
-    "7*6",
-    "7*5",
-    "6*6",
-    "6*5",
-    "6*4",
-    "5*5",
-    "5*4",
-    "5*3",
-    "4*4"
+    [
+        "По кругу стоит 6 аборигенов. Каждый из них\n говорит “Мой сосед справа — лжец”.\n Сколько среди них лжецов?",
+        "Художник Худобеднов за месяц работы написал\n 42 картины. На 17 из них есть лес,\n на 29 река, а на 13 - и то,\n и другое. На остальных картинах\n - не пойми что. Сколько картин\n изображают не пойми что?"
+    ],
+    [
+        "Нескольким третьеклассниками раздали\n 50 яблок так, чтобы каждый получил\n хотя бы по одному яблоку и ни у каких двух\n не было поровну яблок. Какое наибольшее\n количество третьеклассников могли\n получить яблоки?",
+        "От прямоугольника отрезали квадрат\n (прямым разрезом от края до края)\n и его площадь уменьшилась в 2 раза.\n Найдите площадь исходного прямоугольника,\n если его периметр был равен 60 м."
+    ],
+    [
+        "Художник Худобеднов за месяц работы написал\n 42 картины. На 17 из них есть лес,\n на 29 река, а на 13 - и то, и другое. На остальных\n картинах - не пойми что. Сколько картин\n изображают не пойми что? ",
+        "Сколько получится, если сложить наименьшее\n двузначное, наименьшее трехзначное\n и наименьшее четырехзначное число?"
+    ],
+    [
+        "У Пети 3 разных футболок и 6 разных шорт.\n Каждый день он одевает одну футболку\n и одни шорты так, чтобы не ходить\n в одной одежде два раза. Сколько дней\n он может это делать?",
+        "В ящике лежит 10 красных, 8 синих, 8 зеленых\n и 4 желтых шарика. Сколько надо\n вынуть шариков, чтобы среди них\n наверняка нашелся шарик каждого цвета?"
+    ],
+    [
+        "Лиса Алиса и кот Базилио пришли в харчевню\n Трех пескарей, заказали обед и дали\n хозяину 10 золотых. Тот в качестве\n сдачи вернул им столько денег,\n сколько стоил обед. Лиса заметила,\n что хозяин дал им на 2 золотых\n меньше, чем нужно. Сколько денег\n он должен был вернуть им на самом деле?",
+        "Студент за пять лет учебы сдал 31 экзамен.\n В каждом следующем году он сдавал\n больше экзаменов, чем в предыдущем,\n а на пятом курсе – втрое больше,\n чем на первом. Сколько экзаменов он\n сдал на четвертом курсе?"
+    ],
+    [
+        "От прямоугольника отрезали квадрат\n (прямым разрезом от края до края)\n и его площадь уменьшилась в 2 раза.\n Найдите площадь исходного прямоугольника,\n если его периметр был равен 60 м.",
+        "Деревянный куб, ребро которого равно\n 6 см, покрасили снаружи и разрезали\n на кубики, ребро каждого из которых\n равно 1 см. Сколько маленьких кубиков\n имеют по две закрашенные грани?"
+    ],
+    [
+        "Выписав 6 четных чисел, идущих подряд,\n Вася обнаружил, что самое большое\n из них вдвое больше самого\n маленького. Чему равно самое\n маленькое число?",
+        "На распродаже марок любая почтовая марка\n стоила 10 рублей. При этом к каждым\n десяти купленным маркам одна\n давалась бесплатно, а за каждую сотню\n оплаченных марок еще дарили 5 марок.\n Заплатив все свои деньги за марки\n в этом магазине, Андрей получил\n 200 марок. Сколько у него\n было денег?"
+    ],
+    [
+        "Кенгуру купил конфеты трех видов:\n большие, маленькие и средние.\n Каждая большая конфета стоит 4 монеты,\n средняя — 2 монеты и маленькая — 1 монету.\n За 10 конфет Кенгуру заплатил 16 монет.\n Сколько больших конфет он купил,\n если каждого сорта он купил хотя бы по 1?",
+        "Четырехзначное число начинается\n с цифры 5. Эту цифру переставили\n в конец числа. Полученное число\n оказалось на 747 меньше исходного.\n Какова сумма цифр этого числа?"
+    ],
+    ["7*5", "aaa"],
+    ["6*6", "aaa"],
+    ["6*5", "aaa"],
+    ["6*4", "aaa"],
+    ["5*5", "aaa"],
+    ["5*4", "aaa"],
+    ["5*3", "aaa"],
+    ["4*4", "aaa"]
 ]
 answers = [
-    "81",
-    "72",
-    "63",
-    "64",
-    "56",
-    "48",
-    "49",
-    "42",
-    "35",
-    "36",
-    "30",
-    "24",
-    "25",
-    "20",
-    "15",
-    "16"
+    ["3", "9"],
+    ["10", "200"],
+    ["9", "1110"],
+    ["18", "27"],
+    ["6", "8"],
+    ["200", "48"],
+    ["10", "1780"],
+    ["1", "18"],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""],
+    ["", ""]
 ]
 
 solved = [
@@ -63,18 +81,51 @@ solved = [
     False
 ]
 
+BOARD_SIZE = 3
+SQUARE_SIZE = 140
+EMPTY_SQUARE = BOARD_SIZE ** 2
+LVL = 0
+
 root = Tk()
 root.title("Игра пятнашки")
-
 c = Canvas(root, width=BOARD_SIZE * SQUARE_SIZE,
            height=BOARD_SIZE * SQUARE_SIZE, bg='#808080')
 c.pack()
+
+league = Toplevel()
+league.title("Выбери свой класс!")
+league.geometry("300x120")
+league.after(1, lambda: league.focus_force())
+var = IntVar()
+Radiobutton(league, text='3 класс', variable=var, value=0).pack()
+Radiobutton(league, text='4 класс', variable=var, value=1).pack()
+button1 = Button(league, text="Играть!", command=lambda: choose_lvl())
+button1.pack()
+button2 = Button(league, text="Закрыть", command=lambda: close_game())
+button2.pack()
+
+league.protocol("WM_DELETE_WINDOW", lambda: close_game())
+
+lvl = 0
+
+
+def choose_lvl():
+    global lvl
+    lvl = var.get()
+    root.deiconify()  # Показываем основное окно
+    league.destroy()  # Уничтожаем окно выбора сложности
+
+
+def close_game():
+    league.destroy()  # Уничтожаем окно выбора сложности
+    root.destroy()  # Уничтожаем основное окно
+    sys.exit()  # Выходим из программы
 
 
 def get_inv_count():
     """ Функция считающая количество перемещений """
     inversions = 0
-    inversion_board = board[:]
+    inversion_board = board_d[LVL][:]
     inversion_board.remove(EMPTY_SQUARE)
     for i in range(len(inversion_board)):
         first_item = inversion_board[i]
@@ -86,7 +137,7 @@ def get_inv_count():
 
 
 def is_solvable():
-    """ Функция определяющая имеет ли головоломка рещение """
+    """ Функция определяющая имеет ли головоломка решение """
     num_inversions = get_inv_count()
     if BOARD_SIZE % 2 != 0:
         return num_inversions % 2 == 0
@@ -100,7 +151,7 @@ def is_solvable():
 
 def get_empty_neighbor(index):
     # получаем индекс пустой клетки в списке
-    empty_index = board.index(EMPTY_SQUARE)
+    empty_index = board_d[LVL].index(EMPTY_SQUARE)
     # узнаем расстояние от пустой клетки до клетки по которой кликнули
     abs_value = abs(empty_index - index)
     # Если пустая клетка над или под клектой на которую кликнули
@@ -127,7 +178,7 @@ def draw_board():
         for j in range(BOARD_SIZE):
             # получаем значение, которое мы должны будем нарисовать
             # на квадрате
-            index = str(board[BOARD_SIZE * i + j])
+            index = str(board_d[LVL][BOARD_SIZE * i + j])
             # если это не клетка которую мы хотим оставить пустой
             if index != str(EMPTY_SQUARE):
                 # рисуем квадрат по заданным координатам
@@ -158,24 +209,27 @@ def show_victory_plate():
 
 
 def show_problem(ind, board_index, empty_index):
+    print(ind, board_index, empty_index)
+
     def check_answer():
         answer = str(ans_entry.get())
         answer = ''.join(answer.split())
-        if answer == answers[ind]:
-            solved[ind] = True
+        print(answer)
+        if answer == answers[ind - 1][lvl]:
+            solved[ind - 1] = True
             res = Label(prob, text="Ответ верный!\nМожешь закрыть это окно", font="Arial 12")
-            res.grid(row=2, column=0, padx=5, pady=5)
+            res.grid(row=3, column=0, padx=5, pady=5)
             # Меняем местами пустую клетку и клетку, по которой кликнули
-            board[board_index], board[empty_index] = board[empty_index], board[board_index]
+            board_d[LVL][board_index], board_d[LVL][empty_index] = board_d[LVL][empty_index], board_d[LVL][board_index]
             # Перерисовываем игровое поле
             draw_board()
             # Если текущее состояние доски соответствует правильному - рисуем сообщение о победе
-            if board == correct_board:
+            if board_d[LVL] == correct_board:
                 # Эту функцию мы добавим позже
                 show_victory_plate()
         else:
             res = Label(prob, text="Ответ не верный!\nПопробуй еще раз!", font="Arial 12")
-            res.grid(row=2, column=0, padx=5, pady=5)
+            res.grid(row=3, column=0, padx=5, pady=5)
 
     prob = Toplevel()
     prob.title("Реши задачу!")
@@ -184,14 +238,13 @@ def show_problem(ind, board_index, empty_index):
     prob.geometry(size + "x" + size)
     prob.after(1, lambda: prob.focus_force())
 
-    problem = Label(prob, text=problems[ind], font="Arial 12")
+    problem = Label(prob, text=problems[ind - 1][lvl], font="Arial 12")
     problem.grid(row=0, column=0, padx=5, pady=5)
     ans_entry = Entry(prob, width=35)
     ans_entry.after(1, lambda: ans_entry.focus_force())
     ans_entry.grid(row=1, column=0, padx=5, pady=5)
-    ans_entry.delete(0, END)
     message_button = Button(prob, text="Проверить", command=check_answer)
-    message_button.grid(row=1, column=1, padx=5, pady=5)
+    message_button.grid(row=2, column=0, padx=5, pady=5)
 
 
 def click(event):
@@ -206,15 +259,15 @@ def click(event):
     empty_index = get_empty_neighbor(board_index)
 
     if board_index != empty_index:
-        if not solved[board[board_index]]:
-            show_problem(board[board_index], board_index, empty_index)
-        if solved[board[board_index]]:
+        if not solved[board_d[LVL][board_index] - 1]:
+            show_problem(board_d[LVL][board_index], board_index, empty_index)
+        if solved[board_d[LVL][board_index] - 1]:
             # Меняем местами пустую клетку и клетку, по которой кликнули
-            board[board_index], board[empty_index] = board[empty_index], board[board_index]
+            board_d[LVL][board_index], board_d[LVL][empty_index] = board_d[LVL][empty_index], board_d[LVL][board_index]
             # Перерисовываем игровое поле
             draw_board()
             # Если текущее состояние доски соответствует правильному - рисуем сообщение о победе
-            if board == correct_board:
+            if board_d[LVL] == correct_board:
                 # Эту функцию мы добавим позже
                 show_victory_plate()
 
@@ -224,10 +277,12 @@ c.pack()
 
 board = list(range(1, EMPTY_SQUARE + 1))
 correct_board = board[:]
-shuffle(board)
-
-while not is_solvable():
-    shuffle(board)
+board_d = [
+            [3, 2, 7, 5, 1, 6, 4, 8, 9],
+            [12, 13, 16, 4, 10, 2, 1, 5, 7, 3, 9, 6, 14, 11, 15, 8]
+          ]
 
 draw_board()
+root.withdraw()  # Скрываем основное окно, пока показываем окно выбора сложности
+
 root.mainloop()
